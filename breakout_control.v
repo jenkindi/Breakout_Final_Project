80d5b60e4a66b453ee7581a6c229584baaa331db
jenkindi <jenkindi@miamioh.edu>
2025-11-17 17:51:50 -0500

    Add module declaration for breakout_control

--- breakout_control.v
+++ breakout_control.v
@@ -1,1 +1,3 @@
+module breakout_control(clk_50, strt, rst, ,L, R, VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS, VGA_BLANK, VGA_SYNC, VGA_CLK);
 
+vga_adapter display(rst, clk_50, color, x, y, 1'b1, VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS, VGA_BLANK, VGA_SYNC, VGA_CLK);
