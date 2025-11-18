db9933c8ac0537b386a715dae241b9a92e34324c
will1650 <will1650@miamioh.edu>
2025-11-18 12:43:08 -0500

    Update Game_Controller.v

--- Game_Controller.v
+++ Game_Controller.v
@@ -1,3 +1,4 @@
+
 module Game_Controller(
   	//////////// ADC //////////
 	//output		          		ADC_CONVST,
@@ -97,8 +98,8 @@ module Game_Controller(
 
 wire active_pixels; // is on when we're in the active draw space
 
-wire [9:0]x; // current x
-wire [9:0]y; // current y - 10 bits = 1024 ... a little bit more than we need
+//wire [9:0]x; // current x
+//wire [9:0]y; // current y - 10 bits = 1024 ... a little bit more than we need
 
 wire clk;
 wire rst;
@@ -135,43 +136,10 @@ end
 reg flag;
 reg [23:0] vga_color;
 
-always @(posedge clk or negedge rst)
-begin
-	if (rst == 1'b0)
-	begin
-		vga_color <= 24'hFFFFFF;
-		flag <= 1'b0;
-	end
-	else begin
-    if (active_pixels) begin
-        // We are in the visible region
-
-        if (KEY[3] == 1'b0) begin
-            // Button pressed -> draw blue square
-            
-            if (x >= 100 && x < 180 && y >= 100 && y < 150)
-                vga_color <= 24'h0000FF;       // Blue square
-				else if (x >= 400 && x < 560 && y >= 400 && y < 420)
-					vga_color <= 24'h0000FF;
-				else if (x >= 300 && x < 305 && y >= 300 && y < 305)
-					vga_color <= 24'h0000FF;
-            else
-                vga_color <= 24'hFFA500;       // Orange background
-
-        end else begin
-            // Button not pressed
-            vga_color <= 24'hFFFFFF;           // White background
-        end
-    end else begin
-        // In blanking interval
-        vga_color <= 24'h000000;               // Black or don't care
-    end
-end
-end
 
 reg [3:0] S, NS; 
 reg [9:0] x, y;
-wire hit1_1, hit1_2, hit1_3, hit1_4, hit1_5, hit1_6, hit1_7, hit1_8, hit2_1, hit2_2, hit2_3, hit2_4, hit2_5, hit2_6, hit2_7, hit2_8, hit3_1, hit3_2, hit3_3, hit3_4, hit3_5, hit3_6, hit3_7, hit3_8;
+reg hit1_1, hit1_2, hit1_3, hit1_4, hit1_5, hit1_6, hit1_7, hit1_8, hit2_1, hit2_2, hit2_3, hit2_4, hit2_5, hit2_6, hit2_7, hit2_8, hit3_1, hit3_2, hit3_3, hit3_4, hit3_5, hit3_6, hit3_7, hit3_8;
 always @(*) begin
     hit1_1 = 0; hit1_2 = 0; hit1_3 = 0; hit1_4 = 0;
     hit1_5 = 0; hit1_6 = 0; hit1_7 = 0; hit1_8 = 0;
@@ -181,13 +149,13 @@ always @(*) begin
     hit3_5 = 0; hit3_6 = 0; hit3_7 = 0; hit3_8 = 0;
 end
 
-	display my_display(clk, rst, x, y, ballx, bally, paddlex, paddley, block_1_1x, block_1_1y, block_1_2x, block_1_2y, block_1_3x, block_1_3y, block_1_4x, block_1_4y, block_1_5x, block_1_5y, block_1_6x, block_1_6y, block_1_7x, block_1_7y, block_1_8x, block_1_8y,
+	display my_display(clk, rst, start, x, y, ballx, bally, paddlex, paddley, block_1_1x, block_1_1y, block_1_2x, block_1_2y, block_1_3x, block_1_3y, block_1_4x, block_1_4y, block_1_5x, block_1_5y, block_1_6x, block_1_6y, block_1_7x, block_1_7y, block_1_8x, block_1_8y,
                                                                block_2_1x, block_2_1y, block_2_2x, block_2_2y, block_2_3x, block_2_3y, block_2_4x, block_2_4y, block_2_5x, block_2_5y, block_2_6x, block_2_6y, block_2_7x, block_2_7y, block_2_8x, block_2_8y,
                                                                block_3_1x, block_3_1y, block_3_2x, block_3_2y, block_3_3x, block_3_3y, block_3_4x, block_3_4y, block_3_5x, block_3_5y, block_3_6x, block_3_6y, block_3_7x, block_3_7y, block_3_8x, block_3_8y, 
                                                                hit1_1, hit1_2, hit1_3, hit1_4, hit1_5, hit1_6, hit1_7, hit1_8, hit2_1, hit2_2, hit2_3, hit2_4, hit2_5, hit2_6, hit2_7, hit2_8, hit3_1, hit3_2, hit3_3, hit3_4, hit3_5, hit3_6, hit3_7, hit3_8,
                color);
 	
-paramater
+parameter
 START = 4'd0,
 PLAY = 4'd1,
 
@@ -249,4 +217,3 @@ block_3_8x = 10'd560,
 block_3_8y = 10'd120;
 
 endmodule
-
