a304f5e50edd2c7cc1fcf40b3bf39121a929bc5f
jenkindi <jenkindi@miamioh.edu>
2025-11-18 12:27:44 -0500

    Add display function to Game_Controller
    Added display function call with parameters for game state.

--- Game_Controller.v
+++ Game_Controller.v
@@ -180,6 +180,12 @@ always @(*) begin
     hit3_1 = 0; hit3_2 = 0; hit3_3 = 0; hit3_4 = 0;
     hit3_5 = 0; hit3_6 = 0; hit3_7 = 0; hit3_8 = 0;
 end
+
+	display my_display(clk, rst, x, y, ballx, bally, paddlex, paddley, block_1_1x, block_1_1y, block_1_2x, block_1_2y, block_1_3x, block_1_3y, block_1_4x, block_1_4y, block_1_5x, block_1_5y, block_1_6x, block_1_6y, block_1_7x, block_1_7y, block_1_8x, block_1_8y,
+                                                               block_2_1x, block_2_1y, block_2_2x, block_2_2y, block_2_3x, block_2_3y, block_2_4x, block_2_4y, block_2_5x, block_2_5y, block_2_6x, block_2_6y, block_2_7x, block_2_7y, block_2_8x, block_2_8y,
+                                                               block_3_1x, block_3_1y, block_3_2x, block_3_2y, block_3_3x, block_3_3y, block_3_4x, block_3_4y, block_3_5x, block_3_5y, block_3_6x, block_3_6y, block_3_7x, block_3_7y, block_3_8x, block_3_8y, 
+                                                               hit1_1, hit1_2, hit1_3, hit1_4, hit1_5, hit1_6, hit1_7, hit1_8, hit2_1, hit2_2, hit2_3, hit2_4, hit2_5, hit2_6, hit2_7, hit2_8, hit3_1, hit3_2, hit3_3, hit3_4, hit3_5, hit3_6, hit3_7, hit3_8,
+               color);
 	
 paramater
 START = 4'd0,
