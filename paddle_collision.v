e0babe8c0f35fe3e25a78c07430142eb25d47237
jenkindi <jenkindi@miamioh.edu>
2025-11-24 13:12:53 -0500

    Refactor paddle collision logic for clarity

--- paddle_collision.v
+++ paddle_collision.v
@@ -3,6 +3,7 @@ input clk, rst, start;
 input [9:0] ballx, bally, paddlex, paddley;
 output reg paddle_col;
 output reg [2:0] ball_angle;
+	
 
 reg [1:0] S,NS;
 
@@ -34,6 +35,7 @@ begin
     if(rst == 1'b0)
     begin
         paddle_col <= 1'd0;
+		ball_angle <= 3'd3;
     end
     else
     begin
@@ -41,54 +43,54 @@ begin
             START:
                 begin
                   paddle_col <= 1'd0;
+				  ball_angle <= 3'd3;
                 end
             GO: 
-              if(ballx >= paddlex && ballx < paddlex + 20 && bally == pady)
-					           begin
-                        paddle_col <= 1'b1;
-                        ball_angle <= 3'd0;
-					           end 
-              else
-              if(ballx >= paddlex + 20 && ballx < paddlex + 40 && bally == pady)
-					           begin
-                        paddle_col <= 1'b1;
-                        ball_angle <= 3'd1;
-					           end 
-              else
-              if(ballx >= paddlex + 40 && ballx < paddlex + 70 && bally == pady)
-					           begin
-                        paddle_col <= 1'b1;
-                        ball_angle <= 3'd2;
-					           end 
-              else
-              if(ballx >= paddlex + 70 && ballx < paddlex + 90 && bally == pady)
-					           begin
-                        paddle_col <= 1'b1;
-                        ball_angle <= 3'd3;
-					           end 
-              else
-              if(ballx >= paddlex + 90 && ballx < paddlex + 120 && bally == pady)
-					           begin
-                        paddle_col <= 1'b1;
-                        ball_angle <= 3'd4;
-					           end 
-              else    
-              if(ballx >= paddlex + 120 && ballx < paddlex + 140 && bally == pady)
-					           begin
-                        paddle_col <= 1'b1;
-                        ball_angle <= 3'd5;
-					           end 
-              else
-              if(ballx >= paddlex + 140 && ballx < paddlex + 160 && bally == pady)
-					           begin
-                        paddle_col <= 1'b1;
-                        ball_angle <= 3'd6;
-					           end 
-              else
+			 if(ballx >= paddlex && ballx < paddlex + 20 && bally == paddley)
+				begin
+                    paddle_col <= 1'b1;
+                    ball_angle <= 3'd0;
+				end 
+             else
+			 if(ballx >= paddlex + 20 && ballx < paddlex + 40 && bally == paddley)
+				begin
+                    paddle_col <= 1'b1;
+                    ball_angle <= 3'd1;
+				end 
+             else
+		     if(ballx >= paddlex + 40 && ballx < paddlex + 70 && bally == paddley)
+				begin
+                    paddle_col <= 1'b1;
+                    ball_angle <= 3'd2;
+				end 
+             else
+			 if(ballx >= paddlex + 70 && ballx < paddlex + 90 && bally == paddley)
+				begin
+                    paddle_col <= 1'b1;
+                    ball_angle <= 3'd3;
+				end 
+             else
+			 if(ballx >= paddlex + 90 && ballx < paddlex + 120 && bally == paddley)
+				begin
+                	paddle_col <= 1'b1;
+               		ball_angle <= 3'd4;
+				end 
+             else    
+			 if(ballx >= paddlex + 120 && ballx < paddlex + 140 && bally == paddley)
+				begin
+                    paddle_col <= 1'b1;
+                    ball_angle <= 3'd5;
+				end 
+             else
+			 if(ballx >= paddlex + 140 && ballx < paddlex + 160 && bally == paddley)
+				begin
+                    paddle_col <= 1'b1;
+                    ball_angle <= 3'd6;
+				end 
+             else
                  begin
-                     paddle_col <= 1'b0;
-                     ball_angle <= ball_angle;     
-				        end
+                     paddle_col <= 1'b0;    
+				end
           endcase
 		end
 endmodule
