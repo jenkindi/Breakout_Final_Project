0dc5154d3186ed5f57ed803f99f77ec20854da7d
jenkindi <jenkindi@miamioh.edu>
2025-11-25 11:46:59 -0500

    Update vga_driver_memory.v

--- vga_driver_memory.v
+++ vga_driver_memory.v
@@ -102,10 +102,14 @@ wire [9:0]y; // current y - 10 bits = 1024 ... a little bit more than we need
 wire clk;
 wire rst;
 wire start;
+wire left_move;
+wire right_move;
 
 assign clk = CLOCK_50;
-assign rst = KEY[0];
-assign start = SW[1];
+assign rst = KEY[3];
+assign start = SW[0];
+assign left_move = KEY[1];
+assign right_move = KEY[0];
 
 assign LEDR[0] = active_pixels;
 assign LEDR[1] = flag;
@@ -134,12 +138,16 @@ begin
 end
 
 reg flag;
-wire [23:0] color;
 reg [23:0] vga_color;
 
 reg [3:0] S, NS; 
 
+// Instantiate game clock
+wire game_clock
+new_clock my_new_clock(clk, rst, game_clock);
+	
 // Instantiate display module
+wire [23:0] color;
 display my_display(clk, rst, start, x, y, ballx, bally, paddlex, paddley, block_1_1x, block_1_1y, block_1_2x, block_1_2y, block_1_3x, block_1_3y, block_1_4x, block_1_4y, block_1_5x, block_1_5y, block_1_6x, block_1_6y, block_1_7x, block_1_7y, block_1_8x, block_1_8y,
                                                                block_2_1x, block_2_1y, block_2_2x, block_2_2y, block_2_3x, block_2_3y, block_2_4x, block_2_4y, block_2_5x, block_2_5y, block_2_6x, block_2_6y, block_2_7x, block_2_7y, block_2_8x, block_2_8y,
                                                                block_3_1x, block_3_1y, block_3_2x, block_3_2y, block_3_3x, block_3_3y, block_3_4x, block_3_4y, block_3_5x, block_3_5y, block_3_6x, block_3_6y, block_3_7x, block_3_7y, block_3_8x, block_3_8y, 
@@ -149,7 +157,7 @@ display my_display(clk, rst, start, x, y, ballx, bally, paddlex, paddley, block_
 // Instantiate paddle movements
 wire [9:0]paddlex;
 wire [9:0]paddley;
-paddle_move my_paddle_move(clk, rst, start, left_move, right_move, paddlex, paddley);
+paddle_move my_paddle_move(clk, rst, game_clock, start, left_move, right_move, paddlex, paddley);
 
 // Instantiate paddle collision
 wire paddle_col;
@@ -160,6 +168,11 @@ paddle_collision my_paddle_collision(clk, rst, start, ballx, bally, paddlex, pad
 wire wall_col;
 wire ceiling_col;
 boundary_collision my_boundary_collision(clk, rst, start, ballx, bally, wall_col, ceiling_col);
+
+// Instantiate ball movements
+wire [9:0]ballx;
+wire [9:0]bally;
+ball_move my_ball_move(clk, rst, game_clock, start, blockTB_col_1_1, blockTB_col_1_2, blockTB_col_1_3, blockTB_col_1_4, blockTB_col_1_5, blockTB_col_1_6, blockTB_col_1_7, blockTB_col_1_8, blockTB_col_2_1, blockTB_col_2_2, blockTB_col_2_3, blockTB_col_2_4, blockTB_col_2_5, blockTB_col_2_6, blockTB_col_2_7, blockTB_col_2_8, blockTB_col_3_1, blockTB_col_3_2, blockTB_col_3_3, blockTB_col_3_4, blockTB_col_3_5, blockTB_col_3_6, blockTB_col_3_7, blockTB_col_3_8, blockLR_col_1_1, blockLR_col_1_2, blockLR_col_1_3, blockLR_col_1_4, blockLR_col_1_5, blockLR_col_1_6, blockLR_col_1_7, blockLR_col_1_8, blockLR_col_2_1, blockLR_col_2_2, blockLR_col_2_3, blockLR_col_2_4, blockLR_col_2_5, blockLR_col_2_6, blockLR_col_2_7, blockLR_col_2_8, blockLR_col_3_1, blockLR_col_3_2, blockLR_col_3_3, blockLR_col_3_4, blockLR_col_3_5, blockLR_col_3_6, blockLR_col_3_7, blockLR_col_3_8, wall_col, ceiling_col, paddle_col, ball_angle, ballx, bally);
 
 // Instantiate block boundary collisions
 wire hit1_1, hit1_2, hit1_3, hit1_4, hit1_5, hit1_6, hit1_7, hit1_8, hit2_1, hit2_2, hit2_3, hit2_4, hit2_5, hit2_6, hit2_7, hit2_8, hit3_1, hit3_2, hit3_3, hit3_4, hit3_5, hit3_6, hit3_7, hit3_8;
@@ -276,6 +289,7 @@ block_3_8y = 10'd120;
 
 
 endmodule
+
 
 
 
