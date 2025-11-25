5fee10fc5f6f255d55144999b94b25a576d3bd10
jenkindi <jenkindi@miamioh.edu>
2025-11-25 11:40:26 -0500

    Create new_clock.v

---
+++ new_clock.v
@@ -0,0 +1,29 @@
+module new_clock(clk, rst, game_clock);
+
+input clk, rst;
+output reg game_clock;
+
+  reg [25:0] counter;
+
+always@(posedge clk or negedge rst)
+begin
+	if(rst == 1'b0)
+  	begin
+  		counter <= 26'd0;
+  		game_clock <= 1'b0;
+  	end
+	else
+	begin
+    if (counter >= 26'416667) //120 pixels per second
+  		begin
+  			counter <= 26'd0;
+  			game_clock <=1'b1;
+  		end
+		else
+  		begin
+  			counter <= counter + 1'b1;
+  			game_clock <= 1'b0;
+  		end
+  end
+end
+endmodule
