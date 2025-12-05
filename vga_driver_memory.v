2e5801447ac012337a20decc9915016a2c33e699
will1650 <will1650@miamioh.edu>
2025-12-05 10:15:05 -0500

    Update vga_driver_memory.v

--- vga_driver_memory.v
+++ vga_driver_memory.v
@@ -106,13 +106,19 @@ wire left_move;
 wire right_move;
 
 assign clk = CLOCK_50;
-assign rst = KEY[2];
-assign start = SW[0];
+assign rst = KEY[3];
+assign start = ~KEY[2];
 assign left_move = KEY[1];
 assign right_move = KEY[0];
 
 assign LEDR[0] = active_pixels;
-assign LEDR[1] = flag;
+assign LEDR[9] = blockTB_col_3_1;
+assign LEDR[3] = wall_col;
+assign LEDR[8] = hit3_1;
+assign LEDR[7] = blockLR_col_3_1;
+
+assign LEDR[6] = paddle_col;
+
 
 vga_driver the_vga(
 .clk(clk),
@@ -177,6 +183,8 @@ paddle_collision my_paddle_collision(clk, rst, start, ballx, bally, paddlex, pad
 // Instantiate boundary collision
 boundary_collision my_boundary_collision(clk, rst, start, ballx, bally, wall_col, ceiling_col);
 
+
+	
 // Instantiate ball movements
 ball_move my_ball_move(clk, rst, game_clock, start, blockTB_col_1_1, blockTB_col_1_2, blockTB_col_1_3, blockTB_col_1_4, blockTB_col_1_5, blockTB_col_1_6, blockTB_col_1_7, blockTB_col_1_8, blockTB_col_2_1, blockTB_col_2_2, blockTB_col_2_3, blockTB_col_2_4, blockTB_col_2_5, blockTB_col_2_6, blockTB_col_2_7, blockTB_col_2_8, blockTB_col_3_1, blockTB_col_3_2, blockTB_col_3_3, blockTB_col_3_4, blockTB_col_3_5, blockTB_col_3_6, blockTB_col_3_7, blockTB_col_3_8, blockLR_col_1_1, blockLR_col_1_2, blockLR_col_1_3, blockLR_col_1_4, blockLR_col_1_5, blockLR_col_1_6, blockLR_col_1_7, blockLR_col_1_8, blockLR_col_2_1, blockLR_col_2_2, blockLR_col_2_3, blockLR_col_2_4, blockLR_col_2_5, blockLR_col_2_6, blockLR_col_2_7, blockLR_col_2_8, blockLR_col_3_1, blockLR_col_3_2, blockLR_col_3_3, blockLR_col_3_4, blockLR_col_3_5, blockLR_col_3_6, blockLR_col_3_7, blockLR_col_3_8, wall_col, ceiling_col, paddle_col, ball_angle, ballx, bally, lose);
 
@@ -230,31 +238,32 @@ localparam
 	block_3_7y = 10'd120, 
 	block_3_8x = 10'd560, 
 	block_3_8y = 10'd120;
-	
-block_collision block_1_1(clk, rst, start, ballx, bally, block_1_1x, block_1_1y, hit1_1, blockTB_col_1_1, blockLR_col_1_1);
-block_collision block_1_2(clk, rst, start, ballx, bally, block_1_2x, block_1_2y, hit1_2, blockTB_col_1_2, blockLR_col_1_2);
-block_collision block_1_3(clk, rst, start, ballx, bally, block_1_3x, block_1_3y, hit1_3, blockTB_col_1_3, blockLR_col_1_3);
-block_collision block_1_4(clk, rst, start, ballx, bally, block_1_4x, block_1_4y, hit1_4, blockTB_col_1_4, blockLR_col_1_4);
-block_collision block_1_5(clk, rst, start, ballx, bally, block_1_5x, block_1_5y, hit1_5, blockTB_col_1_5, blockLR_col_1_5);
-block_collision block_1_6(clk, rst, start, ballx, bally, block_1_6x, block_1_6y, hit1_6, blockTB_col_1_6, blockLR_col_1_6);
-block_collision block_1_7(clk, rst, start, ballx, bally, block_1_7x, block_1_7y, hit1_7, blockTB_col_1_7, blockLR_col_1_7);
-block_collision block_1_8(clk, rst, start, ballx, bally, block_1_8x, block_1_8y, hit1_8, blockTB_col_1_8, blockLR_col_1_8);
-block_collision block_2_1(clk, rst, start, ballx, bally, block_2_1x, block_2_1y, hit2_1, blockTB_col_2_1, blockLR_col_2_1);
-block_collision block_2_2(clk, rst, start, ballx, bally, block_2_2x, block_2_2y, hit2_2, blockTB_col_2_2, blockLR_col_2_2);
-block_collision block_2_3(clk, rst, start, ballx, bally, block_2_3x, block_2_3y, hit2_3, blockTB_col_2_3, blockLR_col_2_3);
-block_collision block_2_4(clk, rst, start, ballx, bally, block_2_4x, block_2_4y, hit2_4, blockTB_col_2_4, blockLR_col_2_4);
-block_collision block_2_5(clk, rst, start, ballx, bally, block_2_5x, block_2_5y, hit2_5, blockTB_col_2_5, blockLR_col_2_5);
-block_collision block_2_6(clk, rst, start, ballx, bally, block_2_6x, block_2_6y, hit2_6, blockTB_col_2_6, blockLR_col_2_6);
-block_collision block_2_7(clk, rst, start, ballx, bally, block_2_7x, block_2_7y, hit2_7, blockTB_col_2_7, blockLR_col_2_7);
-block_collision block_2_8(clk, rst, start, ballx, bally, block_2_8x, block_2_8y, hit2_8, blockTB_col_2_8, blockLR_col_2_8);
-block_collision block_3_1(clk, rst, start, ballx, bally, block_3_1x, block_3_1y, hit3_1, blockTB_col_3_1, blockLR_col_3_1);
-block_collision block_3_2(clk, rst, start, ballx, bally, block_3_2x, block_3_2y, hit3_2, blockTB_col_3_2, blockLR_col_3_2);
-block_collision block_3_3(clk, rst, start, ballx, bally, block_3_3x, block_3_3y, hit3_3, blockTB_col_3_3, blockLR_col_3_3);
-block_collision block_3_4(clk, rst, start, ballx, bally, block_3_4x, block_3_4y, hit3_4, blockTB_col_3_4, blockLR_col_3_4);
-block_collision block_3_5(clk, rst, start, ballx, bally, block_3_5x, block_3_5y, hit3_5, blockTB_col_3_5, blockLR_col_3_5);
-block_collision block_3_6(clk, rst, start, ballx, bally, block_3_6x, block_3_6y, hit3_6, blockTB_col_3_6, blockLR_col_3_6);
-block_collision block_3_7(clk, rst, start, ballx, bally, block_3_7x, block_3_7y, hit3_7, blockTB_col_3_7, blockLR_col_3_7);
-block_collision block_3_8(clk, rst, start, ballx, bally, block_3_8x, block_3_8y, hit3_8, blockTB_col_3_8, blockLR_col_3_8);
+
+
+block_collision block_1_1(game_clock, rst, start, ballx, bally, block_1_1x, block_1_1y, hit1_1, blockTB_col_1_1, blockLR_col_1_1);
+block_collision block_1_2(game_clock, rst, start, ballx, bally, block_1_2x, block_1_2y, hit1_2, blockTB_col_1_2, blockLR_col_1_2);
+block_collision block_1_3(game_clock, rst, start, ballx, bally, block_1_3x, block_1_3y, hit1_3, blockTB_col_1_3, blockLR_col_1_3);
+block_collision block_1_4(game_clock, rst, start, ballx, bally, block_1_4x, block_1_4y, hit1_4, blockTB_col_1_4, blockLR_col_1_4);
+block_collision block_1_5(game_clock, rst, start, ballx, bally, block_1_5x, block_1_5y, hit1_5, blockTB_col_1_5, blockLR_col_1_5);
+block_collision block_1_6(game_clock, rst, start, ballx, bally, block_1_6x, block_1_6y, hit1_6, blockTB_col_1_6, blockLR_col_1_6);
+block_collision block_1_7(game_clock, rst, start, ballx, bally, block_1_7x, block_1_7y, hit1_7, blockTB_col_1_7, blockLR_col_1_7);
+block_collision block_1_8(game_clock, rst, start, ballx, bally, block_1_8x, block_1_8y, hit1_8, blockTB_col_1_8, blockLR_col_1_8);
+block_collision block_2_1(game_clock, rst, start, ballx, bally, block_2_1x, block_2_1y, hit2_1, blockTB_col_2_1, blockLR_col_2_1);
+block_collision block_2_2(game_clock, rst, start, ballx, bally, block_2_2x, block_2_2y, hit2_2, blockTB_col_2_2, blockLR_col_2_2);
+block_collision block_2_3(game_clock, rst, start, ballx, bally, block_2_3x, block_2_3y, hit2_3, blockTB_col_2_3, blockLR_col_2_3);
+block_collision block_2_4(game_clock, rst, start, ballx, bally, block_2_4x, block_2_4y, hit2_4, blockTB_col_2_4, blockLR_col_2_4);
+block_collision block_2_5(game_clock, rst, start, ballx, bally, block_2_5x, block_2_5y, hit2_5, blockTB_col_2_5, blockLR_col_2_5);
+block_collision block_2_6(game_clock, rst, start, ballx, bally, block_2_6x, block_2_6y, hit2_6, blockTB_col_2_6, blockLR_col_2_6);
+block_collision block_2_7(game_clock, rst, start, ballx, bally, block_2_7x, block_2_7y, hit2_7, blockTB_col_2_7, blockLR_col_2_7);
+block_collision block_2_8(game_clock, rst, start, ballx, bally, block_2_8x, block_2_8y, hit2_8, blockTB_col_2_8, blockLR_col_2_8);
+block_collision block_3_1(game_clock, rst, start, ballx, bally, block_3_1x, block_3_1y, hit3_1, blockTB_col_3_1, blockLR_col_3_1);
+block_collision block_3_2(game_clock, rst, start, ballx, bally, block_3_2x, block_3_2y, hit3_2, blockTB_col_3_2, blockLR_col_3_2);
+block_collision block_3_3(game_clock, rst, start, ballx, bally, block_3_3x, block_3_3y, hit3_3, blockTB_col_3_3, blockLR_col_3_3);
+block_collision block_3_4(game_clock, rst, start, ballx, bally, block_3_4x, block_3_4y, hit3_4, blockTB_col_3_4, blockLR_col_3_4);
+block_collision block_3_5(game_clock, rst, start, ballx, bally, block_3_5x, block_3_5y, hit3_5, blockTB_col_3_5, blockLR_col_3_5);
+block_collision block_3_6(game_clock, rst, start, ballx, bally, block_3_6x, block_3_6y, hit3_6, blockTB_col_3_6, blockLR_col_3_6);
+block_collision block_3_7(game_clock, rst, start, ballx, bally, block_3_7x, block_3_7y, hit3_7, blockTB_col_3_7, blockLR_col_3_7);
+block_collision block_3_8(game_clock0., rst, start, ballx, bally, block_3_8x, block_3_8y, hit3_8, blockTB_col_3_8, blockLR_col_3_8);
 
 	
 always @(posedge clk or negedge rst)
@@ -266,20 +275,19 @@ begin
 	end
 	else begin
     if (active_pixels) begin
-        // We are in the visible region
-
-        if (KEY[3] == 1'b0) begin
-            // Button pressed -> draw blue square
-            
-            vga_color <= color;      // Orange background
-
+	 
+        if (SW[0] == 1'b1) begin 
+		  
+            vga_color <= color;
+				
         end else begin
-            // Button not pressed
-            vga_color <= 24'hFFFFFF;           // White background
+		  
+            vga_color <= 24'hFFFFFF;           
         end
+		  
     end else begin
-        // In blanking interval
-        vga_color <= 24'h000000;               // Black or don't care
+        
+        vga_color <= 24'h000000;               
     end
 end
 end
